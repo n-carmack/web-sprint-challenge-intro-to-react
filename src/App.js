a1c58492169d9f620830988e9e5ba12bd6850445
Nicholas Carmack <dragecarmack@gmail.com>
2022-12-07 17:01:01 -0700

    completed mvp and added some styled aspects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import "./App.css";
+
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [data, setData] = useState();
 
+  useEffect(() =>{
+    axios.get('https://swapi.dev/api/people/')
+      .then(res => {
+        setData(res.data);
+      })
+      .catch(err => console.error(err))
+  }, [])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -11,8 +24,8 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      { data && <Character list={data} />}
     </div>
   );
 }
-
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import styled from 'styled-components';
+
+const CharacterWrapperDiv = styled.div`
+
+`;
+const CharacterNameDiv = styled.div`
+    margin: auto;
+    border: solid black;
+    color: black;
+    width: 25%;
+    padding: 1rem;
+    font-weight: bold
+
+`;
+
+const Character = (props) =>{
+    return (
+        props.list.map(character=>(
+            <CharacterNameDiv>{character.name} Height:{character.height}</CharacterNameDiv>
+        ))
+            
+    )
+}
+
+export default Character;
